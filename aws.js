aa372b19b38d776cbb78047f7fb62c1f6d32a8d0
Dan Hoffman <dan@criticalhitlabs.com>
2014-06-25 10:41:15 -0700

    initial commit

---
+++ aws.js
@@ -0,0 +1,57 @@
+var AWS 	= require('aws-sdk');
+var logger  = require('./logger');
+
+AWS.config.update({region: 'us-east-1'});
+
+AWS.config.getCredentials(function(err) {
+  if (err) logger.log("error", err.stack); // credentials not loaded
+});
+
+var elasticache = new AWS.ElastiCache();
+
+var refreshElasticacheAddresses = function(callback){
+	var params = {
+	  // Marker: 'STRING_VALUE',
+	  // MaxRecords: 0,
+	  ReplicationGroupId: process.env.AWS_ELASTICACHE_REPLICATION_GROUP_ID
+	};
+	var result 			= {};
+	var primaryEndpoint = {};
+	var readEndpoints 	= [];
+
+	elasticache.describeReplicationGroups(params, function(err, data) {
+		if (err) {
+	  		logger.log('error', err); // an error occurred
+	  		logger.log('error', err.stack);
+	  		callback(err);
+		}
+		else{
+			// successful response
+			// console.log("data = " + JSON.stringify(data, null, 4)); 
+
+			var nodeGroup = data.ReplicationGroups[0].NodeGroups[0];
+
+		  	primaryEndpoint = nodeGroup.PrimaryEndpoint
+
+		  	var members = nodeGroup.NodeGroupMembers
+		  	for (var i = 0; i < members.length; i++) {
+		  		var read = {};
+		  		read = members[i].ReadEndpoint;
+		  		readEndpoints.push(read);
+		  	};
+
+		  	// logger.log("debug", "primary endpoint = " + JSON.stringify(primaryEndpoint, null, 4));
+		  	// logger.log("debug", "read endpoints   = " + JSON.stringify(readEndpoints, null, 4));
+
+		  	var result = {
+		  		"primaryEndpoint": primaryEndpoint,
+		  		"readEndpoints": readEndpoints
+		  	};
+
+		  	callback(null, result);
+		} 
+	});
+}
+
+exports.refreshElasticacheAddresses = refreshElasticacheAddresses;
+
---
+++ redis_provider.js
@@ -0,0 +1,37 @@
+var url     = require('url');
+var logger  = require('./logger');
+
+if (process.env.AWS_ELASTICACHE_REPLICATION_GROUP_ID) {
+    var AWS = require('./aws.js');
+}
+
+function getRedisURL(callback) {
+  var redisUrl;
+  // if REDISTOGO_URL is present, use that
+  if(process.env.REDISTOGO_URL){
+    logger.log("info", "redis provider = redis to go");
+    redisUrl = url.parse(process.env.REDISTOGO_URL);
+    callback(null, redisUrl);
+  } 
+  // if AWS_ELASTICACHE_REPLICATION_GROUP_ID is present, use it
+  else if (process.env.AWS_ELASTICACHE_REPLICATION_GROUP_ID) {
+    logger.log("info", "redis provider =  AWS elasticache");
+    var writeEndpoint = null;
+    AWS.refreshElasticacheAddresses(function(err, result){
+      if (err) return callback(err);
+      writeEndpoint = result.primaryEndpoint;
+      // logger.log("debug", "writeEndpoint = ");
+      // logger.log("debug", writeEndpoint);
+      redisUrl = "http://" + writeEndpoint.Address + writeEndpoint.Port;
+      callback(null, redisUrl);
+    });
+  }
+  else {
+    // otherwise use localhost
+    logger.log("info", "redis provider = localhost");
+    redisUrl = "redis://localhost:6379/";
+    callback(null, redisUrl);
+  }
+};
+
+exports.getRedisURL = getRedisURL;
